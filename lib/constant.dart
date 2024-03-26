45d219ed9e9f453bef4a059ee74bff59a914c2c4
TiBo <thithip@gmail.com>
2024-03-26 09:22:08 +0100

    feat(capture): check if camera is available before trying to capture
    On some devices (desktop, web, iOS emulators) camera can be unavailable, and the app display a verbose error.
    With this feature, we display an error for the User, and the way to return the Home page (thanks to the Scaffold).

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 const MaterialColor DEFAULT_COLOR = Colors.indigo;
-const String API_HOSTNAME = '10.0.2.2:5000';
+String API_HOSTNAME = Platform.isAndroid ? '10.0.2.2:5000' : '0.0.0.0:5000';
 const bool API_IS_HTTPS = false;
