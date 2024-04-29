e30c78c320eb4628b45c75b7765887f28698f4ae
Aline Bonnet <aline.bonnet@otterways.com>
2024-04-29 15:07:21 +0200

    feat: change the UI of photo upload screen

--- lib/page/collection_creation_page.dart
+++ lib/page/collection_creation_page.dart
@@ -13,7 +13,6 @@ class CollectionCreationPage extends StatefulWidget {
 
 class _CarouselWithIndicatorState extends State<CollectionCreationPage> {
   int _current = 0;
-  final CarouselController _carouselController = CarouselController();
   final collectionNameTextController = TextEditingController(
     text: 'My collection ${DateFormat(DATE_FORMATTER).format(DateTime.now())}',
   );
@@ -25,175 +24,131 @@ class _CarouselWithIndicatorState extends State<CollectionCreationPage> {
     super.dispose();
   }
 
-  void goToHomePage() {
-    GetIt.instance<NavigationService>().pushTo(Routes.homepage, arguments: availableCameras);
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.top]);
+    super.initState();
+  }
+
+  void goToCapturePage() async {
+    await availableCameras().then(
+      (availableCameras) =>
+          GetIt.instance<NavigationService>().pushTo(Routes.newSequenceCapture, arguments: availableCameras),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PanoramaxAppBar(context: context),
-        body: Form(
-          key: _formKey,
-          child: Scrollbar(
-            child: CustomScrollView(
-              slivers: [
-                SliverFillRemaining(
-                  hasScrollBody: false,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.fromLTRB(16, 64, 16, 16),
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              border: const UnderlineInputBorder(),
-                              labelText: AppLocalizations.of(context)!.newSequenceNameField_placeholder,
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter some text';
-                              }
-                              return null;
-                            },
-                            controller: collectionNameTextController,
-                          )),
-                      Flexible(
-                        fit: FlexFit.tight,
-                        child: CarouselSlider(
-                          items: buildImageSlider(),
-                          carouselController: _carouselController,
-                          options: CarouselOptions(
-                            autoPlay: true,
-                            enlargeCenterPage: true,
-                            aspectRatio: 2.0,
-                            onPageChanged: (index, reason) {
-                              setState(() {
-                                _current = index;
-                              });
-                            },
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: widget.imgList.asMap().entries.map(
-                          (entry) {
-                            return GestureDetector(
-                              onTap: () => _carouselController.animateToPage(entry.key),
-                              child: Container(
-                                width: 12.0,
-                                height: 12.0,
-                                margin: const EdgeInsets.symmetric(
-                                  vertical: 8.0,
-                                  horizontal: 4.0,
-                                ),
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: (Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black)
-                                      .withOpacity(
-                                    _current == entry.key ? 0.9 : 0.4,
+        backgroundColor: BLUE,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 40, bottom: 40),
+            child: Column(
+              children: [
+                Text(
+                    AppLocalizations.of(context)!.nameSerie(
+                        DateFormat(DATE_FORMATTER).format(DateTime.now())),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    )),
+                Expanded(
+                    child: SingleChildScrollView(
+                        padding: const EdgeInsets.all(16.0),
+                        child: GridView.count(
+                          physics: NeverScrollableScrollPhysics(),
+                          padding: const EdgeInsets.all(10.0),
+                          shrinkWrap: true,
+                          crossAxisCount: 3,
+                          mainAxisSpacing: 5,
+                          crossAxisSpacing: 5,
+                          childAspectRatio: (16 / 9),
+                          children: widget.imgList
+                              .map(
+                                (item) => GridTile(
+                                  child: Stack(
+                                    children: <Widget>[
+                                      Image.file(
+                                        item,
+                                      ),
+                                    ],
                                   ),
                                 ),
-                              ),
-                            );
-                          },
-                        ).toList(),
+                              )
+                              .toList(),
+                        ))),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 64, 0, 32),
+                  child: LoadingBtn(
+                    height: 50,
+                    borderRadius: 8,
+                    animate: true,
+                    color: Colors.blue,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    loader: Container(
+                      padding: const EdgeInsets.all(10),
+                      width: 40,
+                      height: 40,
+                      child: const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 64, 0, 32),
-                        child: LoadingBtn(
-                          height: 50,
-                          borderRadius: 8,
-                          animate: true,
-                          width: MediaQuery.of(context).size.width * 0.45,
-                          loader: Container(
-                            padding: const EdgeInsets.all(10),
-                            width: 40,
-                            height: 40,
-                            child: const CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                            ),
-                          ),
-                          child: Text(AppLocalizations.of(context)!.newSequenceSendButton),
-                          onTap: (startLoading, stopLoading, btnState) async {
-                            if (_formKey.currentState!.validate() && btnState == ButtonState.idle) {
-                              startLoading();
-                              // call your network api
-                              await submitNewCollection(
-                                collectionName: collectionNameTextController.text,
-                                picturesToUpload: widget.imgList,
-                              );
-                              stopLoading();
-                            }
-                          },
-                        ),
-                      ),
-                    ],
+                    ),
+                    child: Text(
+                      AppLocalizations.of(context)!.newSequenceSendButton,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: (startLoading, stopLoading, btnState) async {
+                      if (_formKey.currentState!.validate() &&
+                          btnState == ButtonState.idle) {
+                        startLoading();
+                        // call your network api
+                        await submitNewCollection(
+                          collectionName: collectionNameTextController.text,
+                          picturesToUpload: widget.imgList,
+                        );
+                        stopLoading();
+                      }
+                    },
                   ),
                 ),
+                GestureDetector(
+                    onTap: goToCapturePage,
+                    child: Container(
+                        padding: const EdgeInsets.only(bottom: 1.0),
+                        decoration: BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                                    width: 1.0, color: Colors.white))),
+                        child: Text(
+                          AppLocalizations.of(context)!.newSequenceCancel,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ))),
               ],
             ),
           ),
         ));
   }
 
-  Future<void> submitNewCollection({required String collectionName, required List<File> picturesToUpload}) {
-    return CollectionsApi.INSTANCE.apiCollectionsCreate(newCollectionName: collectionName).then((createdCollection) {
+  Future<void> submitNewCollection(
+      {required String collectionName, required List<File> picturesToUpload}) {
+    return CollectionsApi.INSTANCE
+        .apiCollectionsCreate(newCollectionName: collectionName)
+        .then((createdCollection) {
       debugPrint('Created Collection $createdCollection');
       picturesToUpload.asMap().forEach((index, pictureToUpload) async {
         await CollectionsApi.INSTANCE
             .apiCollectionsUploadPicture(
-                collectionId: createdCollection.id, position: index + 1, pictureToUpload: pictureToUpload)
+                collectionId: createdCollection.id,
+                position: index + 1,
+                pictureToUpload: pictureToUpload)
             .then((value) {
           debugPrint('Picture ${index + 1} uploaded');
         }).catchError((error) => throw Exception(error));
       });
-      goToHomePage();
+      goToCapturePage();
     }).catchError((error) => throw Exception(error));
-  }
-
-  List<Widget> buildImageSlider() {
-    return widget.imgList
-        .map(
-          (item) => Container(
-            margin: const EdgeInsets.all(5.0),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-              child: Stack(
-                children: <Widget>[
-                  Image.file(item, fit: BoxFit.cover, width: 1000.0),
-                  Positioned(
-                    bottom: 0.0,
-                    left: 0.0,
-                    right: 0.0,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                        ),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 10.0,
-                        horizontal: 20.0,
-                      ),
-                      child: Text(
-                        'No. ${widget.imgList.indexOf(item) + 1} image',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        )
-        .toList();
   }
 }
