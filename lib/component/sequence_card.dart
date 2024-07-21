4650c3e2ff0fccd7b7821da720a26648ec8cf02a
Aline Bonnet <aline.bonnet@otterways.com>
2024-07-21 07:31:36 +0200

    refactor: split the code of sequence card into smaller components

--- lib/component/sequence_card.dart
+++ lib/component/sequence_card.dart
@@ -45,71 +45,36 @@ class _SequenceCardState extends State<SequenceCard> {
       ),
       child: Column(
         children: [
-          widget.sequence.geovisio_status == "ready"
-              ? Container(
-                  height: 140,
-                  decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(18),
-                        topRight: Radius.circular(18),
-                      ),
-                      image: DecorationImage(
-                        image:
-                            Image.network(widget.sequence.getThumbUrl()!).image,
-                        fit: BoxFit.cover,
-                      )))
-              : Container(
-                  height: 140,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(18),
-                      topRight: Radius.circular(18),
-                    ),
-                  ),
-                  child: Container(
-                      child: Center(
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                        widget.sequence.geovisio_status == "preparing"
-                            ? Icon(
-                                Icons.check_circle_outline,
-                                color: Colors.blue,
-                                size: 60,
-                              )
-                            : CircularProgressIndicator(
-                                strokeWidth: 4, // thickness of the circle
-                                color: Colors.blue, // color of the progress bar
-                              ),
-                        Text(widget.sequence.geovisio_status == "preparing"
-                            ? AppLocalizations.of(context)!.sendingCompleted
-                            : AppLocalizations.of(context)!.sendingInProgress)
-                      ])))),
-          Container(
-              margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '$itemCount ${AppLocalizations.of(context)!.pictures}',
-                        style: GoogleFonts.nunito(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Shooting(),
-                  widget.sequence.geovisio_status == "ready"
-                      ? Publishing()
-                      : Container()
-                ],
-              )),
+          widget.sequence.geovisio_status == "ready" ? Picture() : Loader(),
+          PictureDetail(),
         ],
       ),
     );
+  }
+
+  Widget PictureDetail() {
+    return Container(
+        margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '$itemCount ${AppLocalizations.of(context)!.pictures}',
+                  style: GoogleFonts.nunito(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+            Shooting(),
+            widget.sequence.geovisio_status == "ready"
+                ? Publishing()
+                : Container()
+          ],
+        ));
   }
 
   Widget Shooting() {
@@ -140,5 +105,49 @@ class _SequenceCardState extends State<SequenceCard> {
         Text(date == null ? "" : dateFormat.format(DateTime.parse(date)))
       ],
     );
+  }
+
+  Widget Loader() {
+    return Container(
+        height: 140,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(18),
+            topRight: Radius.circular(18),
+          ),
+        ),
+        child: Container(
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+              widget.sequence.geovisio_status == "preparing"
+                  ? Icon(
+                      Icons.check_circle_outline,
+                      color: Colors.blue,
+                      size: 60,
+                    )
+                  : CircularProgressIndicator(
+                      strokeWidth: 4, // thickness of the circle
+                      color: Colors.blue, // color of the progress bar
+                    ),
+              Text(widget.sequence.geovisio_status == "preparing"
+                  ? AppLocalizations.of(context)!.sendingCompleted
+                  : AppLocalizations.of(context)!.sendingInProgress)
+            ]))));
+  }
+
+  Widget Picture() {
+    return Container(
+        height: 140,
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(18),
+              topRight: Radius.circular(18),
+            ),
+            image: DecorationImage(
+              image: Image.network(widget.sequence.getThumbUrl()!).image,
+              fit: BoxFit.cover,
+            )));
   }
 }
