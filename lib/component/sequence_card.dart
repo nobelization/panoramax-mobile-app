1208af2d319d42f7df631f1184c35a44a27ef667
Aline Bonnet <aline.bonnet@otterways.com>
2024-07-25 11:42:34 +0200

    feat: automatically updates the upload information of sequences

--- lib/component/sequence_card.dart
+++ lib/component/sequence_card.dart
@@ -1,9 +1,11 @@
 part of panoramax;
 
 class SequenceCard extends StatefulWidget {
-  const SequenceCard(this.sequence, {super.key});
+  const SequenceCard(this.sequence, {this.sequenceCount, super.key});
 
   final GeoVisioLink sequence;
+  final int?
+      sequenceCount; //if sequenceCount is not null, there are photos being uploaded
 
   @override
   State<StatefulWidget> createState() => _SequenceCardState();
@@ -12,17 +14,39 @@ class SequenceCard extends StatefulWidget {
 class _SequenceCardState extends State<SequenceCard> {
   late int itemCount;
   GeoVisioCollectionImportStatus? geovisioStatus;
+  Timer? timer;
 
   @override
   void initState() {
     super.initState();
     itemCount = widget.sequence.stats_items!.count;
-    if (widget.sequence.geovisio_status != "preparing") {
-      getStatus();
+    if (widget.sequence.geovisio_status != "preparing" ||
+        widget.sequenceCount != null) {
+      timer = Timer.periodic(Duration(seconds: 1), (timer) {
+        getStatus();
+      });
     }
   }
 
-  Future<void> getStatus() async {}
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  Future<void> getStatus() async {
+    GeoVisioCollectionImportStatus? geovisioStatusRefresh;
+    try {
+      geovisioStatusRefresh = await CollectionsApi.INSTANCE
+          .getGeovisioStatus(collectionId: widget.sequence.id!);
+    } catch (e) {
+      print(e);
+    } finally {
+      setState(() {
+        geovisioStatus = geovisioStatusRefresh;
+      });
+    }
+  }
 
   Future<void> openUrl() async {
     final instance = await getInstance();
@@ -54,7 +78,10 @@ class _SequenceCardState extends State<SequenceCard> {
       ),
       child: Column(
         children: [
-          widget.sequence.geovisio_status == "ready" ? Picture() : Loader(),
+          widget.sequence.geovisio_status == "ready" &&
+                  widget.sequenceCount == null
+              ? Picture()
+              : Loader(),
           PictureDetail(),
         ],
       ),
@@ -70,23 +97,30 @@ class _SequenceCardState extends State<SequenceCard> {
             Shooting(),
             widget.sequence.geovisio_status == "ready"
                 ? Publishing()
+                : Container(),
+            widget.sequenceCount == null ||
+                    widget.sequence.stats_items?.count == widget.sequenceCount
+                ? Blurring()
                 : Container()
           ],
         ));
   }
 
   Widget PictureCount() {
+    final count =
+        widget.sequenceCount == null ? itemCount : widget.sequenceCount;
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          '$itemCount ${AppLocalizations.of(context)!.pictures}',
+          '$count ${AppLocalizations.of(context)!.pictures}',
           style: GoogleFonts.nunito(
             fontSize: 18,
             fontWeight: FontWeight.w800,
           ),
         ),
-        widget.sequence.geovisio_status == "ready"
+        widget.sequence.geovisio_status == "ready" &&
+                widget.sequenceCount == null
             ? FloatingActionButton(
                 onPressed: openUrl,
                 child: Icon(
@@ -146,7 +180,8 @@ class _SequenceCardState extends State<SequenceCard> {
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-              widget.sequence.geovisio_status == "preparing"
+              widget.sequenceCount == null ||
+                      geovisioStatus?.items.length == widget.sequenceCount
                   ? Icon(
                       Icons.check_circle_outline,
                       color: Colors.blue,
@@ -156,7 +191,8 @@ class _SequenceCardState extends State<SequenceCard> {
                       strokeWidth: 4, // thickness of the circle
                       color: Colors.blue, // color of the progress bar
                     ),
-              Text(widget.sequence.geovisio_status == "preparing"
+              Text(widget.sequenceCount == null ||
+                      geovisioStatus?.items.length == widget.sequenceCount
                   ? AppLocalizations.of(context)!.sendingCompleted
                   : AppLocalizations.of(context)!.sendingInProgress)
             ]))));
@@ -174,5 +210,34 @@ class _SequenceCardState extends State<SequenceCard> {
               image: Image.network(widget.sequence.getThumbUrl()!).image,
               fit: BoxFit.cover,
             )));
+  }
+
+  Widget Blurring() {
+    int count = geovisioStatus?.items
+            .where(
+              (element) => element.status == "ready",
+            )
+            .length ??
+        0;
+    double total = geovisioStatus?.items.length.toDouble() ?? 0;
+    return Container(
+        child: Column(
+      children: [
+        LinearProgressIndicator(
+          value: (total == 0) ? 0 : count / total, //don't divide by 0 !
+          semanticsLabel: AppLocalizations.of(context)!.blurringInProgress,
+          minHeight: 16,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        Row(
+          children: [
+            const Icon(Icons.blur_on_outlined),
+            Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(AppLocalizations.of(context)!.blurringInProgress))
+          ],
+        )
+      ],
+    ));
   }
 }
